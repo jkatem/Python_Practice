c9371e9cd2f3bd002512ae67c46d63a4fac24e93
jkatem <jenmiao@gmail.com>
2021-06-28 19:33:47 -0700

    start palindrome challenge

---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+# CHALLENGE
+
+# Write a python function to determine if a given string is a palindrome
+# Input: String to evaluate
+# Output: Boolean Value
+# Only consider letters A-Z
+# Ignore case
+
+def ifPalindrome(): 
