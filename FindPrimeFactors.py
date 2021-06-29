7995c5efb2d51f2aeb5e6e76c87d1c6a23ffe7cd
jkatem <jenmiao@gmail.com>
2021-06-28 19:14:53 -0700

    solved challenge

--- FindPrimeFactors.py
+++ FindPrimeFactors.py
@@ -1,1 +1,19 @@
-def get_prime_factors
+
+# 1. Find prime factorization of a given number. 
+# function should take in one parameter, integer
+# Returns a list containing all of its prime factors
+
+
+def get_prime_factors(int):
+    factors = list()
+    divisor = 2
+    while(divisor <= int):
+        if (int % divisor) == 0:
+            factors.append(divisor)
+            int = int/divisor
+        else: 
+            divisor += 1
+    return factors 
+
+get_prime_factors(630)
+
